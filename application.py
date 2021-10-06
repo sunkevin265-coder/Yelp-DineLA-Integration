b9aaa47d3f9b5fe0335421dd05d687fef06a6a5f
Kevin Sun <33167525+sunkevin265-coder@users.noreply.github.com>
2021-10-05 23:43:08 -0700

    Added requirements.txt

--- application.py
+++ application.py
@@ -6,6 +6,14 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+#Check for environment variable
+if not os.getenv("DATABASE_URL"):
+	raise RuntimeError("DATABASE_URL is not set")
+
+#Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
 @app.route("/")
 def index(): 
     return render_template("index.html")
