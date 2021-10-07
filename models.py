37092d74041f7369f642cad6eecc39371aedba2b
Kevin Sun <33167525+sunkevin265-coder@users.noreply.github.com>
2021-10-06 21:55:12 -0700

    Updated models.py
    Updated models.py file with a more accurate model of what the tables will look like

--- models.py
+++ models.py
@@ -7,8 +7,15 @@ db = SQLAlchemy()
 
 class DineLA(db.Model):
 	__tablename__ = "dinela"
-	id = db.Column(db.Integer, primary_key = True)
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.Integer, nullable=False)
+	neighborhood = db.Column(db.Integer, nullable=False)
+	lunchPrice = db.Column(db.Integer, nullable=True)
+	dinnerPrice = db.Column(db.Integer, nullable=True)
+
 
 class Yelp(db.Model):
 	__tablename__ = "yelp"
 	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.Integer, nullable=False)
+	rating = db.Column(db.Integer, nullable=False)
