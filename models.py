c8ec039f6070dca0e976869d2174473b69ba863d
Kevin Sun <33167525+sunkevin265-coder@users.noreply.github.com>
2021-10-07 22:37:42 -0700

    Update models.py
    Fixed data type of columns

--- models.py
+++ models.py
@@ -8,15 +8,15 @@ db = SQLAlchemy()
 class DineLA(db.Model):
 	__tablename__ = "dinela"
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.Integer, nullable=False)
-	neighborhood = db.Column(db.Integer, nullable=False)
-	lunchPrice = db.Column(db.Integer, nullable=True)
-	dinnerPrice = db.Column(db.Integer, nullable=True)
-	linkToDineLA = db.Column(db.Integer, nullable=False)
-
+	name = db.Column(db.String, nullable=False)
+	neighborhood = db.Column(db.String, nullable=False)
+	lunchPrice = db.Column(db.String, nullable=True)
+	dinnerPrice = db.Column(db.String, nullable=True)
+	linkToDineLA = db.Column(db.String, nullable=False)
+	cuisine = db.Column(db.String, nullable=False)
 
 class Yelp(db.Model):
 	__tablename__ = "yelp"
 	id = db.Column(db.Integer, primary_key = True)
-	name = db.Column(db.Integer, nullable=False)
-	rating = db.Column(db.Integer, nullable=False)
+	name = db.Column(db.String, nullable=False)
+	rating = db.Column(db.String, nullable=False)
